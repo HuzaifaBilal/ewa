37c30690b4900f5a2ca12df55d8d412e963f6ee3
HuzaifaBilal <huzaifa@pragtex.com>
2024-01-07 15:51:19 +0530

    ewa- basic setup, header and footer.

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <main className="py-3">
+        <Container></Container>
+      </main>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default App;
