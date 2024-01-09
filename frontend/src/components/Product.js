54ec8fdd90457633f66a1558b649386f1a1be037
HuzaifaBilal <huzaifa@pragtex.com>
2024-01-09 19:53:35 +0530

    ewa- feat rating component and product screen.

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`product/${product._id}`}>
+      <Link to={`product/${product._id}`}>
         <Card.Img src={product.image} variant="top"></Card.Img>
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`product/${product._id}`}>
-          <Card.Title as="div">
+        <Link to={`product/${product._id}`}>
+          <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
+          <Card.Text as="div">
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            ></Rating>
+          </Card.Text>
           <Card.Text as="h3">${product.price}</Card.Text>
-        </a>
+        </Link>
       </Card.Body>
     </Card>
   );
