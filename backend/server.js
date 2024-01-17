9e6a47b759d824398979539c21b1f61660d7768d
HuzaifaBilal <huzaifabilal2191@gmail.com>
2024-01-17 23:39:12 +0530

    ewa- feat mongoose setup.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import products from "./data/Products.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
+connectDB(); //connect to mongoose
 const app = express();
 app.get("/", (req, res) => {
   res.send("API is running");
