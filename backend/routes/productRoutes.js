0b00d6d301abfe6de1b4a0bddbac5f62542cc3ba
HuzaifaBilal <huzaifabilal2191@gmail.com>
2024-01-19 19:41:41 +0530

    ewa- feat using data from DB.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,23 @@
 import express from "express";
 const router = express.Router();
-import products from "../data/Products.js";
-router.get("/", (req, res) => {
-  res.json(products);
-});
-router.get("/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+import asyncHandler from "../middlewares/asyncHandler.js";
+import Product from "../models/productModel.js";
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    }
+    res.status(404).json({ message: "product not found." });
+  })
+);
 
 export default router;
