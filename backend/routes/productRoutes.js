8fabc790dfea4ea2ff1ccd3e6841dc6e5c2881d4
HuzaifaBilal <huzaifabilal2191@gmail.com>
2024-01-20 11:22:05 +0530

    ewa- feat controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,25 +1,6 @@
 import express from "express";
+import { getProductById, getProducts } from "../controllers/productController";
 const router = express.Router();
-import asyncHandler from "../middlewares/asyncHandler.js";
-import Product from "../models/productModel.js";
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
-
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 export default router;
